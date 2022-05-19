f08c1a687e242d2260cd917a076b5465d30d4d23
Yahirs19 <L21212055@tectijuana.edu.mx>
2022-05-19 11:38:31 -0700

    Update

--- ProgramaExcepciones/Program.cs
+++ ProgramaExcepciones/Program.cs
@@ -10,7 +10,26 @@ namespace ProgramaExcepciones
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            try
+            {
+                Console.Write("Indique un numero: ");
+                float num1 = float.Parse(Console.ReadLine());
+                Console.Write("Indique otro numero: ");
+                float num2 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //DivideByZeroException
+            //FormatException
         }
     }
 }
